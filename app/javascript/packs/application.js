582533fae6ab6ed9aeb8b1a50b265a121ee228a7
adrien anelli <adrien.anelli.work@gmail.com>
2018-03-29 16:32:47 +0200

    dropdown mobile ok, tabs ok, information page content ok

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,13 @@
 import "bootstrap";
+
+ // Add slideDown animation to Bootstrap dropdown when expanding.
+$('.dropdown').on('show.bs.dropdown', function() {
+  $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+  $('.dropdown-menu-btn-icon').animate({marginTop: "-40px"});
+});
+
+// Add slideUp animation to Bootstrap dropdown when collapsing.
+$('.dropdown').on('hide.bs.dropdown', function() {
+  $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+  $('.dropdown-menu-btn-icon').animate({marginTop: "0"});
+});
