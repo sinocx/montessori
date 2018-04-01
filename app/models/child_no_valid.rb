cae92790b1467cd118eeb1e2882094fc41867969
adrien anelli <adrien.anelli.work@gmail.com>
2018-04-01 11:05:17 +0200

    subscription form part 1 ok

--- app/models/child_no_valid.rb
+++ app/models/child_no_valid.rb
@@ -4,15 +4,16 @@ class ChildNoValid < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :gender, presence: true, inclusion: %w(Masculin Féminin)
-  validates :birth_date, presence: true, format: { with: /\d{2}\/\d{2}\/\d{4}/ }
+  validates :birth_date, presence: true
+  # , format: { with: /\d{2}\-\d{2}\-\d{4}/ }
   validates :nationality, presence: true
   validates :first_lang, presence: true
-  validates :snd_lang, presence: true
-  validates :montessori_before, presence: true, inclusion: { in:[ true, false ]}
+  # validates :snd_lang
+  validates :montessori_before, inclusion: { in:[ true, false ]}
   validates :montessori_name, presence: true, if: :montessori_before?
-  validates :workshop, presence: true, inclusion: { in:[ true, false ]}
-  validates :nursery, presence: true, inclusion: { in:[ true, false ]}
-  validates :comment, presence: true
+  validates :workshop, inclusion: { in:[ true, false ]}
+  validates :nursery, inclusion: { in:[ true, false ]}
+  # validates :comment
 
   def montessori_before?
     montessori_before
