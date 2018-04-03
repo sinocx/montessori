b933a4be8e07bb577e8b4a8c8754cdc91157c939
Adam Sarfati <adamsarfat@hotmail.fr>
2018-04-03 16:30:50 +0200

    changement couleur encore"

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -7,7 +7,7 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
     {
         "stylers": [
             {
-                "saturation": -25
+                "saturation": 0
             },
             {
                 "gamma": 1
