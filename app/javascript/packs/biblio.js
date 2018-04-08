201ad958a74b3725442876b1cc59dff6b305669e
adrien anelli <adrien.anelli.work@gmail.com>
2018-04-08 17:58:52 +0200

    front ok

--- app/javascript/packs/biblio.js
+++ app/javascript/packs/biblio.js
@@ -1,40 +1,13 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
   const books = document.querySelectorAll('.book-overlay');
-  // const wrapper = document.getElementById('biblio');
 
-  // for (var book of books) {
-  //   book.addEventListener('click', function() {
-  //     if (this.classList.contains('selected')) {
-  //       this.classList.remove('selected');
-  //     } else {
-  //       books.forEach(function(book) {
-  //         if (book.classList.contains('selected')) {
-  //           book.classList.remove('selected');
-  //         }
-  //       });
-  //       console.log('adding selected');
-  //       this.classList.add('selected');
-  //     }
-  //   });
-
-    // wrapper.addEventListener('click', function() {
-    //   books.forEach(function(book) {
-    //     if (book.classList.contains('selected')) {
-    //       book.classList.remove('selected');
-    //     }
-    //   });
-    // })
-  // };
   window.onclick = function(event) {
-    console.log(event.target);
     books.forEach(function(book) {
-      console.log(book.children);
       let elmt = [book];
-      for (var child in book.children) {
-        elmt.push(child);
-      };
-      // console.log(elmt);
+      elmt.push(book.children.item(0));
+      elmt.push(book.children.item(1));
+      elmt.push(book.children.item(2));
       if (elmt.includes(event.target) && book.classList.contains('selected')) {
         book.classList.remove('selected');
       } else if (elmt.includes(event.target) && !book.classList.contains('selected')) {
@@ -44,4 +17,5 @@ document.addEventListener("DOMContentLoaded", function(event) {
       }
     });
   };
+
 });
