461ba853ae0e28c868de7be76a482f1b1fdd0e00
Adam Sarfati <adamsarfat@hotmail.fr>
2018-04-21 13:01:45 +0200

    changement accueil ecole et programme ambiance pour les autorisation d'acces

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home,
+  skip_before_action :authenticate_user!, only: [ :ecole,
                                                   :programmes,
                                                   :pedagogie,
                                                   :pedagogie_montessori,
